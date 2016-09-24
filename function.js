6ac8e4abf1567b2ccd6c4f9b38738572c01a77de
danish1u <danishprince786a@gmail.com>
2016-09-24 10:39:08 -0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,5 @@
+            scriptLoader('http://www.pstnow.com/closethedealonline/store/slider/jquery.simpleslider.js', 'http://www.pstnow.com/closethedealonline/store/slider/init-slider.js');
+            (function(c, t) {
+                c = c.split('.');
+                t = t.split('.');
+            })(jQuery.fn.jquery, '1.8.3')
