a11a91aa9fd212da2a380360b256becbd501257b
danish1u <danishprince786a@gmail.com>
2016-10-08 19:39:03 -0700

    Update and rename 0slider-settings.js to 1slider-settings.js

---
+++ store/slider/1slider-settings.js
@@ -0,0 +1,13 @@
+initSlider({
+	// Slide settings
+	target: 'div#CoverImage',
+	interval: 5, // second
+	random: false, // true/false
+	contents: [
+
+		{image: 'https://raw.githubusercontent.com/danish1u/Maureen-eBay/master/Mens%20RFID%20A1.jpg', link: '#'},
+		{image: 'https://raw.githubusercontent.com/danish1u/Maureen-eBay/master/Books%20A4.jpg', link: '#'},
+		{image: 'https://raw.githubusercontent.com/danish1u/Maureen-eBay/master/banner%20cd%20A2.jpg', link: '#'},
+	        {image: 'https://raw.githubusercontent.com/danish1u/Maureen-eBay/master/movies%20and%20music%20A3.jpg', link: '#'}
+	]
+});
